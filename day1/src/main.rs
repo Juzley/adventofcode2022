cb6f4aa0bd2d6492a35b46193c90a90ea4098376
Julian Priestley <juzley@gmail.com>
2022-12-01 16:12:21 +0000

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let file = File::open("input").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut elves = Vec::new();
+    let mut cals = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.is_empty() {
+            elves.push(cals);
+            cals = 0;
+        } else {
+            cals += line.parse::<i64>().unwrap();
+        }
+    }
+    elves.sort_by(|a, b| b.cmp(a));
+    println!("{:?}", elves);
+    println!("Part 1: {}, Part 2: {}", elves[0], elves[0] + elves[1] + elves[2]);
+}
